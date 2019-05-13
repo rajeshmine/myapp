e394eb6eaadedb5dea3d386e47fa055c5732abd3
rajeshmine <36945206+rajeshmine@users.noreply.github.com>
2019-05-13 11:56:13 +0530

    LoginPage
    Loginpage Creation and Default route to Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+
+import login from './components/core/Login';
+import dashboard from './components/Dashboard';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="/">
+      <Switch>
+        <Redirect from="/" to="/login" exact />
+        <Route path="/login" component={login} />
+        <Route path="/dashboard" component={dashboard} />
+      </Switch>
+    </Router>
   );
 }
 
